2ccd90b08b1c33e29d33b781300567457881f19d
Orin Fletcher <orin.fletcher@gmail.com>
2017-10-15 22:40:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector('.target')
 }
+
+function deepestChild(){
+  let sel = document.querySelectorAll('#grand-node')
+  console.log(sel)
+}
