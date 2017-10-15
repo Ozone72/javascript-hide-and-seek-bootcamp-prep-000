08aec1aaeb2e6c13a06ef285e1e99e89c0119730
Orin Fletcher <orin.fletcher@gmail.com>
2017-10-15 22:48:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function nestedTarget(){
   return document.querySelector('.target')
 }
 
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+}
+
 function deepestChild(){
   let sel = document.querySelectorAll('#grand-node')
   console.log(sel)
