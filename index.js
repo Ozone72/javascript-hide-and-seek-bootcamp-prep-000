497f7f146398b1e9218bb26cd9b330395d7ca29c
Orin Fletcher <orin.fletcher@gmail.com>
2017-10-15 22:28:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getFirstSelector(selector){
   //let selector = selector
-  return document.querySelector(${selector})
+  return document.querySelector(selector)
 }
 
 getFirstSelector('div')
